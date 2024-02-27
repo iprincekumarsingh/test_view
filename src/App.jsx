916d4f82436f4f50e2a55ef5ec3fee48ac6c5449
Prince <princekumar2000.pks@gmail.com>
2024-02-27 21:33:48 +0530

    fixed  3

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,11 @@ export default function App() {
       clearInterval(typingInterval);
     };
   }, []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div
       className="flex flex-col min-h-screen"
@@ -38,56 +42,63 @@ export default function App() {
       <nav className="text-black">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://flowbite.com/"
+            href="#"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img src={logo} className="h-20" alt="Flowbite Logo" />
           </a>
           <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400  dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+        onClick={toggleMenu}
+        data-collapse-toggle="navbar-default"
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400  dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded={isMenuOpen ? "true" : "false"}
+      >
+        <span className="sr-only">Open main menu</span>
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+
+      <div
+        className={`${
+          isMenuOpen ? 'block' : 'hidden'
+        } w-full md:block md:w-auto`}
+        id="navbar-default"
+      >
+        <ul className="font-medium flex flex-col p-4 items-center md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+          <li className="border border-white py-2 px-2 rounded-md">
+            <a
+              href="#"
+              className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 "
+              aria-current="page"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 items-center md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
-              <li className="border border-white py-2 px-2 rounded-md">
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 "
-                  aria-current="page"
-                >
-                  WhitePaper
-                </a>
-              </li>
-              <li className="border bg-green-500 border-white py-2 px-2 rounded-md">
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700"
-                >
-                  Launch App
-                </a>
-              </li>
-            </ul>
-          </div>
+              WhitePaper
+            </a>
+          </li>
+          <li className="border bg-green-500 border-white py-2 px-2 rounded-md">
+            <a
+              href="#"
+              className="block py-2 px-3 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-blue-500 dark:hover:bg-gray-700"
+            >
+              Launch App
+            </a>
+          </li>
+        </ul>
+      </div>
         </div>
       </nav>
 
@@ -115,15 +126,14 @@ export default function App() {
       <footer className="bg-black rounded-lg shadow m-4">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
-            <a href="https://flowbite.com/" className="hover:underline"></a>.
-            All Rights Reserved.
+            © 2023 <a href="#" className="hover:underline"></a>. All Rights
+            Reserved.
           </span>
           <ul className="flex flex-wrap gap-2  items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
                 <img
-                  className="bg-white rounded-r-md"
+                  className="bg-white rounded-full p-1"
                   src="https://assets-global.website-files.com/6527c1b34fea32f3c88ebc27/652e5dd6d083f812766d5d70_x-icon.svg"
                   alt=""
                 />
@@ -132,7 +142,7 @@ export default function App() {
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
                 <img
-                  className="bg-white roundedmd"
+                  className="bg-white rounded-full p-1"
                   src="https://assets-global.website-files.com/6527c1b34fea32f3c88ebc27/652e5dd419b525ccffe13db5_telegram-icon%20(1).svg"
                   alt=""
                 />
@@ -141,7 +151,7 @@ export default function App() {
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
                 <img
-                  className="bg-white roundedmd"
+                  className="bg-white rounded-full p-1"
                   src="https://assets-global.website-files.com/6527c1b34fea32f3c88ebc27/652e5dd5582082213dc673b9_discord-icon%20(1).svg"
                   alt=""
                 />
